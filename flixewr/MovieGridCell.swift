3b4e0c4bea70175d175f545d72aaf60742f6798f
Hamidreza Safakhoo <50091982+hsafakho@users.noreply.github.com>
2020-02-01 23:55:35 -0800

    assignment 2 done

---
+++ flixewr/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixewr
+//
+//  Created by Kay Lab on 2/1/20.
+//  Copyright © 2020 projectt11. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
